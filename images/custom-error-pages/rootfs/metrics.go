6e83bb8d814d04c206100e4bf36d31f501f1bae4
Georgi Panov <77702912+Darkfella91@users.noreply.github.com>
2024-11-18 23:33:38 +0200

    Update metrics.go

--- images/custom-error-pages/rootfs/metrics.go
+++ images/custom-error-pages/rootfs/metrics.go
@@ -1,48 +1,22 @@
-/*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Collect and display Prometheus metrics
-
 package main
 
 import (
     "github.com/prometheus/client_golang/prometheus"
 )
 
-const (
-    namespace = "default_http_backend"
-    subsystem = "http"
-)
-
 var (
     requestCount = prometheus.NewCounterVec(
         prometheus.CounterOpts{
-            Namespace: namespace,
-            Subsystem: subsystem,
-            Name:      "request_count_total",
-            Help:      "Total number of HTTP requests made.",
+            Name: "http_requests_total",
+            Help: "Total number of HTTP requests",
         },
         []string{"proto"},
     )
-
     requestDuration = prometheus.NewHistogramVec(
         prometheus.HistogramOpts{
-            Namespace: namespace,
-            Subsystem: subsystem,
-            Name:      "request_duration_seconds",
-            Help:      "Histogram of the duration (in seconds) of HTTP requests.",
-            Buckets:   prometheus.DefBuckets,
+            Name:    "http_request_duration_seconds",
+            Help:    "Duration of HTTP requests",
+            Buckets: prometheus.DefBuckets,
         },
         []string{"proto"},
     )
